335fc5e8049d6adf993c8394475500cc8fc8c009
Algarp2026 <Office@elementourbano.com>
2026-05-19 21:59:01 +0100

    hehe

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.THE_VIEW_CONFIG = {
-  GOOGLE_SHEETS_WEBAPP_URL: "https://script.google.com/macros/s/AKfycbzBGq9zryffsEkG5JPp6lRwy9vsQzJ24NdNRbWVploD0tyqbnj-zBC-6PfKo3XgIbnrgQ/exec"
+  GOOGLE_SHEETS_WEBAPP_URL: "https://script.google.com/macros/s/AKfycbxnIOPMPMPrXuLT1bpjtPWJ4hppIImPlTxQNDtR8oz7cCZ37JxsxMmY5XF2a4UZhOUHzQ/exec"
 };
