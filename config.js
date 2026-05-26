0df2ee9dcd2f8f62db0ed75b57ffe4b5bc6371f6
Algarp2026 <Office@elementourbano.com>
2026-05-26 10:44:07 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.THE_VIEW_CONFIG = {
-  GOOGLE_SHEETS_WEBAPP_URL: "https://script.google.com/macros/s/AKfycbz079cCZ9ZJE4j_viz3LU-4IvQBGx-zHZAi2gdXkfHyel2qgETa_jSA6XIC-w7hmnlu9Q/exec"
+  GOOGLE_SHEETS_WEBAPP_URL: "https://script.google.com/macros/s/AKfycbws8Jvh8ZytPtYdFs71PIqxHgpMp41mjR0q-zRiQG-brCDMFxPCPAr0a0Li_wl2ZUPUwA/exec"
 };
